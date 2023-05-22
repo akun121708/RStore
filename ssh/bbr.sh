f112430a85d872430d4c2b44cc7a037803750505
akun121708 <130338209+akun121708@users.noreply.github.com>
2023-05-22 10:22:24 +0700

    Update bbr.sh

--- ssh/bbr.sh
+++ ssh/bbr.sh
@@ -13,7 +13,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/akun121708/izinvps/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
